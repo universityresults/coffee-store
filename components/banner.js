db84689aea73df8968d4e11415de14afadf82740
root <root@vps.server.local>
2022-09-15 17:08:46 +0000

    any test

--- components/banner.js
+++ components/banner.js
@@ -6,7 +6,7 @@ const Banner = (props) => {
     <div className={styles.container}>
       <h1 className={styles.title}>
         <span className={styles.title1}>Coffee</span>
-        <span className={styles.title2}>Connoisseurrrrrrrrr</span>
+        <span className={styles.title2}>Connoisseur</span>
       </h1>
       <p className={styles.subTitle}>Discovr your local coffee shop!</p>
       <div className={styles.buttonWrapper}>
